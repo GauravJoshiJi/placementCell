edc4e5653f90aebac9a89cb8bd489766b2a980c7
GauravJoshiJi <gauravjoshi1997.joshi@gmail.com>
2023-09-11 16:11:41 +0530

    First Commit

---
+++ config/mongoose.js
@@ -0,0 +1,32 @@
+// requiring the library
+const mongoose = require("mongoose");
+
+//connecting to the database
+// mongoose.connect(
+
+mongoose.connect(
+  "mongodb+srv://csvuploader:TiLkLdTlgsqJcQHb@cluster0.vqn8roh.mongodb.net/csvuploader?retryWrites=true&w=majority"
+);
+
+// acquiring the connection to check if it is succesfull
+const db = mongoose.connection;
+
+// checking for the error
+db.on("error", console.error.bind(console, "error in connecting the database"));
+
+// up and running then print the statement
+db.once("open", () => {
+  console.log("succesfully connected to database");
+});
+
+// exporting the connection
+module.exports = db;
+
+// Password Authentication
+// userName: gauravjoshi
+// password: jWOql2mXRi6y9dxm
+
+//UseraName: gauravjoshi
+// Password: goZU3QaJx9dWfyat
+
+// mongodb+srv://gauravjoshi:<password>@cluster0.5qxyzzt.mongodb.net/?retryWrites=true&w=majority
